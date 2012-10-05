6550daf3cd59631f4c36d70646fbc9f0c29e5d74
psi <ledyba@users.sourceforge.jp>
2012-10-05 10:37:59 +0900

    WidgetWrappaerLayout

--- src/chisa/tk/World.cpp
+++ src/chisa/tk/World.cpp
@@ -55,6 +55,9 @@ void World::init(weak_ptr<World> _self)
 	if(shared_ptr<Universe> universe = this->universe_.lock()){
 		this->layoutFactory_ = new layout::LayoutFactory(this->log_, _self, universe->resolveWorldFilepath(this->name_, "layout.xml"));
 	}
+
+
+	this->pushLayout("main");
 }
 
 void World::render()
