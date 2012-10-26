95e8b12997a0b1a56ccb07ff7c2266e5265b8aab
psi <ledyba@users.sourceforge.jp>
2012-10-26 16:56:20 +0900

    リファクタ
    Signed-off-by: psi <ledyba@users.sourceforge.jp>

--- src/chisa/util/internal/FileUtil.h
+++ src/chisa/util/internal/FileUtil.h
@@ -27,6 +27,7 @@ namespace util {
 namespace file {
 
 namespace internal {
+
 template <class String>
 struct FileConstants ;
 
@@ -42,21 +43,19 @@ struct FileConstants ;
 typedef char* char_type;\
 typedef const char* const_char_type;\
 typedef std::string string_type;\
+typedef const std::string const_string_type;\
 static constexpr const_char_type Sep=(CHISA_FILE_SEP);\
-static constexpr const_char_type CurrentDir = (".");\
-static constexpr const_char_type ParentDir = ("..");\
-static const string_type CurrentDirStr;\
-static const string_type ParentDirStr;
+static constexpr const_char_type CurrentDir = ".";\
+static constexpr const_char_type ParentDir = "..";
 
 #define WSTRING_INNER \
 typedef wchar_t* char_type;\
 typedef const wchar_t* const_char_type;\
 typedef std::wstring string_type;\
+typedef const std::wstring const_string_type;\
 static constexpr const_char_type Sep = CHISA_FILE_SEPW;\
 static constexpr const_char_type CurrentDir = L".";\
-static constexpr const_char_type ParentDir = L"..";\
-static const string_type CurrentDirStr;\
-static const string_type ParentDirStr;\
+static constexpr const_char_type ParentDir = L"..";
 
 template<> struct FileConstants<std::string>{ STRING_INNER };
 template<> struct FileConstants<std::wstring> { WSTRING_INNER };
@@ -64,11 +63,17 @@ template<> struct FileConstants<std::wstring> { WSTRING_INNER };
 template<> struct FileConstants<char> { STRING_INNER };
 template<> struct FileConstants<wchar_t> { WSTRING_INNER };
 
+template<> struct FileConstants<char*> { STRING_INNER };
+template<> struct FileConstants<wchar_t*> { WSTRING_INNER };
+
 template<std::size_t N> struct FileConstants<char[N]> { STRING_INNER };
 template<std::size_t N> struct FileConstants<wchar_t [N]> { WSTRING_INNER };
 
 #undef STRING_INNER
 #undef WSTRING_INNER
+#undef CHISA_FILE_SEP
+#undef CHISA_FILE_SEPW
+
 }
 
 template <typename T>
