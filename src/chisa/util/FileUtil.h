852501a364ac9c8a93105f2e9a04c691b11a0189
psi <ledyba@users.sourceforge.jp>
2012-10-26 16:07:09 +0900

    リファクタ
    Signed-off-by: psi <ledyba@users.sourceforge.jp>

--- src/chisa/util/FileUtil.h
+++ src/chisa/util/FileUtil.h
@@ -21,69 +21,16 @@
 #include "Platform.h"
 #include <string>
 #include <set>
+#include "FileUtilInternal.h"
 
 namespace chisa {
 namespace util {
 namespace file {
 
-namespace internal {
-template <class String>
-struct FileUtil ;
-
-template<>
-struct FileUtil<std::string>
+template <typename T, typename... Args>
+constexpr typename internal::FileUtil<T>::string_type join(const T& a, const Args&... left)
 {
-	typedef const char* char_type;
-	typedef const std::string string_type;
-#if CHISA_WINDOWS
-	static constexpr char_type Sep="\\";
-#else
-	static constexpr char_type Sep = "/";
-#endif
-	static constexpr char_type CurrentDir = (".");
-	static constexpr char_type ParentDir = ("..");
-	static string_type CurrentDirStr;
-	static string_type ParentDirStr;
-};
-template<>
-
-struct FileUtil<std::wstring>
-{
-	typedef const wchar_t* char_type;
-	typedef const std::wstring string_type;
-#if CHISA_WINDOWS
-	static constexpr char_type Sep = L"\\";
-#else
-	static constexpr char_type Sep = L"/";
-#endif
-	static constexpr char_type CurrentDir = L".";
-	static constexpr char_type ParentDir = L"..";
-	static string_type CurrentDirStr;
-	static string_type ParentDirStr;
-};
-
-}
-
-inline const std::string& join(const std::string& a)
-{
-	return a;
-}
-inline const std::wstring& join(const std::wstring& a)
-{
-	return a;
-}
-
-template <typename... Args>
-std::string join(const std::string& a, const Args&... left)
-{
-	using namespace internal;
-	return *(a.end()-1) != FileUtil<std::string>::Sep[0] ? a+FileUtil<std::string>::Sep+join(left...) : a+join(left...);
-}
-template <typename... Args>
-std::wstring join(const std::wstring& a, const Args&... left)
-{
-	using namespace internal;
-	return *(a.end()-1) != FileUtil<std::wstring>::Sep[0] ? a+FileUtil<std::wstring>::Sep+join(left...) : a+join(left...);
+	return typename internal::FileUtil<T>::string_type(a)+internal::FileUtil<T>::Sep+join(left...);
 }
 
 void enumFiles(const std::string& dir, std::set<std::string>& list, bool recursive=false);
