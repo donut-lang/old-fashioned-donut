516e1160ec5b47ed249d2e143a86f07d483dbfde
psi <ledyba@users.sourceforge.jp>
2012-11-07 10:06:01 +0900

    little fix

--- makeParser.sh
+++ makeParser.sh
@@ -12,19 +12,16 @@ if [ $MSYSTEM = MINGW32 ] ; then
 	ANTLR=${ANTLR} \
 	cmd << EOF
 set CLASSPATH=%CURDIR%\external\antlr\antlr-3.4-complete.jar;%CLASSPATH%
-java org.antlr.Tool -fo %ANTLR_OUT% -make %ANTLR_SRC%/Donut.g %ANTLR_SRC%/Compiler.g
-
-for /F "usebackq" %%s in (\`echo %ANTLR_OUT% ^\| sed s_/_\\\\\_g\`) do set ANTLR_OUT_W=%%s
-ren %ANTLR_OUT_W%\*.c *.cpp
+java org.antlr.Tool -fo %ANTLR_OUT% -make %ANTLR_SRC%\Donut.g %ANTLR_SRC%\Compiler.g
 EOF
 else
 	CLASSPATH=${ANTLR}:$CLASSPATH \
 	java org.antlr.Tool -fo ${ANTLR_OUT} -make \
 		${ANTLR_SRC}/Donut.g ${ANTLR_SRC}/Compiler.g
-	
-	find ${ANTLR_OUT} -type f -name *.c | while read f; do
-	  mv $f ${ANTLR_OUT}/$(basename $f .c).cpp
-	done
 fi
+
+find ${ANTLR_OUT} -type f -name *.c | while read f; do
+  mv $f ${ANTLR_OUT}/$(basename $f .c).cpp
+done
 
 
