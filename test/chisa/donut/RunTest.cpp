d39e5b6901fa1fabd9f5050e242db13b48945fda
psi <ledyba@users.sourceforge.jp>
2012-11-08 16:34:35 +0900

    だいたいできた。あとはクロージャですよクロージャーーーーーーーーーーーーー
    Signed-off-by: psi <ledyba@users.sourceforge.jp>

---
+++ test/chisa/donut/RunTest.cpp
@@ -0,0 +1,51 @@
+/**
+ * Chisa
+ * Copyright (C) 2012 psi
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "../../TestCommon.h"
+#include "../../../src/chisa/donut/parser/Parser.h"
+#include "../../../src/chisa/donut/object/World.h"
+#include "../../../src/chisa/donut/vm/Machine.h"
+#include <math.h>
+
+namespace chisa {
+namespace donut {
+
+class DonutRunTest : public ::testing::Test
+{
+protected:
+	Handler<Code> code;
+public:
+	void SetUp(){
+		code = Handler<Code>(new Code());
+	}
+	void TearDown(){
+		code.reset();
+	}
+};
+TEST_F(DonutRunTest, BasicTest)
+{
+	unsigned int idx = Parser::fromString("test=1;", "<MEM>", 0)->parseProgram(code);
+	World world(log_trace, code);
+	Machine machine(log_trace, &world);
+
+	machine.start(idx);
+}
+
+}}
+
+
