0d22da370e1670956a214f28e21a6f02251251f5
psi <ledyba@users.sourceforge.jp>
2012-11-16 11:23:31 +0900

    メモリや逆アセンブラを書くための高速な文字レンダリングクラス
    Signed-off-by: psi <ledyba@users.sourceforge.jp>

---
+++ src/chisa/gl/PredefinedStringRenderer.h
@@ -0,0 +1,43 @@
+/**
+ * Chisa
+ * Copyright (C) 2012 psi
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+#include "../util/ClassUtil.h"
+#include "../geom/Decl.h"
+#include "DrawableManager.h"
+
+namespace chisa {
+namespace gl {
+class Canvas;
+
+class PredefinedStringRenderer {
+	DISABLE_COPY_AND_ASSIGN(PredefinedStringRenderer);
+private:
+	Handler<DrawableManager> drawableManager_;
+	std::map<unsigned int, Handler<Sprite> > spriteTable_;
+public:
+	typedef std::basic_string<unsigned int> String;
+	static const constexpr unsigned int EOS = 0U;
+	PredefinedStringRenderer(Handler<DrawableManager> drawableManager);
+	virtual ~PredefinedStringRenderer() noexcept = default;
+public:
+	void registerCharacter( unsigned int symbol, const std::string& str );
+	void renderString( Canvas& cv, const geom::Point& point, const String& str );
+};
+
+}}
