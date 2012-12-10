634e96e7f64b2d74b9724d9d48baa1ccea858357
psi <ledyba@users.sourceforge.jp>
2012-12-10 18:20:23 +0900

    カバレッジを取るスクリプト

---
+++ cov.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+#! /bin/sh
+
+function filter() {
+	Pattern=$1
+	lcov -r cov.info "${Pattern}" -o cov_tmp
+	mv cov_tmp cov.info
+}
+
+lcov -c -f -b build/debug -d build/debug -o cov.info
+filter "*/usr/*"
+filter "*/external/*"
+rm cov/ -rf
+genhtml -o cov -f cov.info
+rm cov.info
+find . -type f -name *.gcov | xargs rm
+
