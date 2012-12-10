693f283a80cee71572d821f8c1c11c8aaf7ac381
psi <ledyba@users.sourceforge.jp>
2012-12-10 10:47:26 +0900

    add test

--- test/chisa/donut/ObjectTest.cpp
+++ test/chisa/donut/ObjectTest.cpp
@@ -31,6 +31,7 @@ protected:
 public:
 	void SetUp(){
 		donut = Handler<Donut>(new Donut(log_trace));
+		donut->bootstrap();
 		clock = donut->clock();
 		heap = donut->heap();
 	}
@@ -38,9 +39,47 @@ public:
 	}
 };
 
-TEST_F(ObjectTest, NopTest)
+TEST_F(ObjectTest, CastTest)
 {
-//	Object::
+	{
+		const object_desc_t val = 10;
+		const object_desc_t restored = Object::castToDescriptor ( Object::castToPointer(val) );
+		ASSERT_EQ(val, restored);
+	}
+	{
+		const object_desc_t val = (object_desc_t)-1;
+		const object_desc_t restored = Object::castToDescriptor ( Object::castToPointer(val) );
+		ASSERT_EQ(val, restored);
+	}
+}
+
+TEST_F(ObjectTest, ObjectIdEncodingTest)
+{
+	{
+		const objectid_t val = 10;
+		const objectid_t restored = Object::decodeObjectId( Object::encodeObjectId(val) );
+		ASSERT_EQ(val, restored);
+	}
+	{
+		const objectid_t val = (objectid_t)-1;
+		const objectid_t restored = Object::decodeObjectId( Object::encodeObjectId(val) );
+		ASSERT_EQ(val, restored);
+	}
+}
+
+TEST_F(ObjectTest, ObjectPrimitiveCheckTest)
+{
+	ASSERT_TRUE( Object::isPrimitiveDescriptor(heap->createInt(1)->toDescriptor()) );
+	ASSERT_TRUE( Object::isPrimitiveDescriptor(heap->createInt(-1)->toDescriptor()) );
+	ASSERT_TRUE( Object::isPrimitiveDescriptor(heap->createBool(true)->toDescriptor()) );
+	ASSERT_TRUE( Object::isPrimitiveDescriptor(heap->createBool(false)->toDescriptor()) );
+	ASSERT_TRUE( Object::isPrimitiveDescriptor(heap->createNull()->toDescriptor()) );
+
+	ASSERT_FALSE( Object::isPrimitiveDescriptor(heap->createDonutObject()->toDescriptor()) );
+	ASSERT_FALSE( Object::isPrimitiveDescriptor(heap->createEmptyDonutObject()->toDescriptor()) );
+	ASSERT_FALSE( Object::isPrimitiveDescriptor(heap->createFloatObject(10.1)->toDescriptor()) );
+	ASSERT_FALSE( Object::isPrimitiveDescriptor(heap->createFloatObject(NAN)->toDescriptor()) );
+	ASSERT_FALSE( Object::isPrimitiveDescriptor(heap->createStringObject("str")->toDescriptor()) );
 }
 
 }}
