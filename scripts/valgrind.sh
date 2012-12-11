a74ac07baf62d04b4e04028f35132567601f8a36
psi <ledyba@users.sourceforge.jp>
2012-12-11 11:12:07 +0900

    fix

--- scripts/valgrind.sh
+++ scripts/valgrind.sh
@@ -1,8 +1,10 @@
 #! /bin/sh
-DIRNAME=$(cd $(dirname $0);pwd)
-PRG=${DIRNAME}/Linux/debug/chisa_test
+DIRNAME=$(cd $(dirname $0);cd ..;pwd)
+PRG=${DIRNAME}/build/debug/chisa_test
 GEN=--gen-suppressions=all
 
-#valgrind -v --track-origins=yes --error-limit=no --leak-check=yes --show-reachable=no ${PRG} 2>&1 | tee ${DIRNAME}/valgrind.log
-valgrind -v --error-limit=no --leak-check=full --show-reachable=yes --suppressions=_valgrind_sup.txt ${GEN} ${PRG} 2>&1 | tee ${DIRNAME}/valgrind.log
+#valgrind -v --track-origins=yes --error-limit=no --leak-check=yes\
+--show-reachable=no ${PRG} 2>&1 | gedit -
+valgrind -v --error-limit=no --leak-check=full --show-reachable=yes \
+--suppressions=${DIRNAME}/scripts/_valgrind_sup.txt ${GEN} ${PRG} 2>&1 | gedit -
 
