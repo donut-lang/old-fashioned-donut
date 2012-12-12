f49542a2b2ebf3d0cdd60e7e845c351e0902a05f
psi <ledyba@users.sourceforge.jp>
2012-12-12 14:40:32 +0900

    ビルド環境改善

---
+++ bld.sh
@@ -0,0 +1,39 @@
+#! /bin/sh
+DIRNAME=$(cd $(dirname $0);pwd)
+
+function conf() {
+	BUILDDIR=$DIRNAME/build
+	if [ "$MSYSTEM" = "MINGW32" -o "$OS" = "Windows_NT" ] ; then
+		CONFIGURE_CMD="python waf configure --out $BUILDDIR --boost-includes=/d/software/dev/applib/boost/include --boost-lib=/d/software/dev/applib/boost/lib"
+	else
+		export set CC='ccache gcc'
+		export set CXX='ccache g++'
+		CONFIGURE_CMD="python waf configure --out $BUILDDIR"
+	fi
+	$CONFIGURE_CMD
+}
+
+MODE=$1
+if [ "$MODE" = "debug" ] ; then
+	shift
+elif [ "$MODE" = "release" ] ; then
+	shift
+else
+	MODE="debug"
+fi
+
+TARGET=$1
+if [ -z $TARGET ] ; then
+TARGET="build"
+fi
+shift
+
+BLD_WAF_CMD="python waf --out $BUILDDIR --progress ${TARGET}_${MODE} $*"
+$BLD_WAF_CMD 2>&1
+if [ $? -ne 0 ] ; then
+	echo "not configured"
+	conf
+	$BLD_WAF_CMD 2>&1
+fi
+exit $?
+
