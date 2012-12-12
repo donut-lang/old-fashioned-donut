8c2db7fea9284467cc780898b9e85cecd66dd5e7
psi <ledyba@users.sourceforge.jp>
2012-12-12 13:48:32 +0900

    ccache導入

--- build.sh
+++ build.sh
@@ -5,6 +5,8 @@ BUILDDIR=$DIRNAME/build
 if [ "$MSYSTEM" = "MINGW32" -o "$OS" = "Windows_NT" ] ; then
 	CONFIGURE_CMD="python waf configure --out $BUILDDIR --boost-includes=/d/software/dev/applib/boost/include --boost-lib=/d/software/dev/applib/boost/lib"
 else
+	export set CC='ccache gcc'
+	export set CXX='ccache g++'
 	CONFIGURE_CMD="python waf configure --out $BUILDDIR"
 fi
 
