e0d9b482d18222f103dbc50f02c27397da0804f3
psi <ledyba@users.sourceforge.jp>
2012-12-17 15:36:48 +0900

    プロファイル用スクリプト

--- scripts/dprof.sh
+++ scripts/dprof.sh
@@ -1,9 +1,8 @@
 #! /bin/sh
-DIRNAME=$(cd $(dirname $0);cd ..;pwd)/build/debug
+DIRNAME=$(cd $(dirname $0);cd ..;pwd)/build/release
 export CPUPROFILE=/tmp/prof.out
-RUN_PROG=${DIRNAME}/chisa
-${RUN_PROG}
+RUN_PROG=${DIRNAME}/donut
+${RUN_PROG} < ${DIRNAME}/../../__donut_sample__/tarai.donut
 pprof --dot ${RUN_PROG} /tmp/prof.out | dot -T png > ${DIRNAME}/prof.png
 rm -f ${CPUPROFILE}
 shotwell  ${DIRNAME}/prof.png
-
