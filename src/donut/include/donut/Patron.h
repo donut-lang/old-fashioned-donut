7d81a84f3175948810d5a3ead3e3da8df4330b77
psi <ledyba@users.sourceforge.jp>
2012-12-24 14:22:53 +0900

    外部との接続を行うためのインターフェイスである「パトロン」の作成

---
+++ src/donut/include/donut/Patron.h
@@ -0,0 +1,38 @@
+/**
+ * Donut
+ * Copyright (C) 2012 psi
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+#include <tarte/Handler.h>
+
+namespace donut {
+using namespace tarte;
+
+class Patron : public HandlerBody<Patron>
+{
+protected:
+	Patron() = default;
+	virtual ~Patron() noexcept = default;
+public:
+	inline bool onFree() const noexcept { return false; };
+public:
+	virtual void onRegisterProvider(Handler<Heap> const& heap) = 0;
+	virtual void onRegisterInitialGlobalObject(Handler<Heap> const& heap) = 0;
+};
+
+}
+
