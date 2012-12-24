a3e5001819b84eaa9e14cb43102dc9fbcc5124a0
psi <ledyba@users.sourceforge.jp>
2012-12-24 11:30:01 +0900

    順番の見直し

--- test/donut/lang/ReactiveObjectTest.cpp
+++ test/donut/lang/ReactiveObjectTest.cpp
@@ -16,9 +16,9 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <math.h>
 #include "DonutHelper.h"
 #include <donut/object/ReactiveNativeObject.h>
-#include <math.h>
 
 namespace donut {
 namespace {
