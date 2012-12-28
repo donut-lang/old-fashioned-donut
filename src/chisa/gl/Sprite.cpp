731007e03907674e482137c74e8234f219a2156f
psi <ledyba@users.sourceforge.jp>
2012-12-28 13:44:31 +0900

    リファクタ

--- src/chisa/gl/Sprite.cpp
+++ src/chisa/gl/Sprite.cpp
@@ -73,24 +73,32 @@ void Sprite::drawImpl(Canvas* const canvas, geom::Point const& ptInRoot, geom::A
 {
 	this->flushBuffer();
 	geom::Area const drawn(geom::Area(geom::ZERO, size_).intersect(mask));
-	geom::Area const drawnInRoot(drawn.point()+ptInRoot, drawn.box());
 	geom::Area coordinate(drawn);
 	coordinate.x(coordinate.x() / this->origSize_.width());
 	coordinate.y(coordinate.y() / this->origSize_.height());
 	coordinate.width(coordinate.width() / this->origSize_.width());
 	coordinate.height(coordinate.height() / this->origSize_.height());
-	canvas->drawTexture(this->texId_, drawn, coordinate, depth, color);
+	canvas->drawTexture(
+			this->texId_,
+			geom::Area(drawn.point()+ptInRoot, drawn.box()),
+			coordinate,
+			depth,
+			color);
 }
 void Sprite::drawImpl(Canvas* const canvas, geom::Point const& ptInRoot, const float depth, Color const& color)
 {
 	this->flushBuffer();
-	geom::Area const drawn(geom::Area(ptInRoot, size_));
 	geom::Area coordinate(geom::Area(geom::ZERO, size_));
 	coordinate.x(coordinate.x() / this->origSize_.width());
 	coordinate.y(coordinate.y() / this->origSize_.height());
 	coordinate.width(coordinate.width() / this->origSize_.width());
 	coordinate.height(coordinate.height() / this->origSize_.height());
-	canvas->drawTexture(this->texId_, drawn, coordinate, depth, color);
+	canvas->drawTexture(
+			this->texId_,
+			geom::Area(ptInRoot, size_),
+			coordinate,
+			depth,
+			color);
 }
 
 void Sprite::flushBuffer()
