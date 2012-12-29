1d58c899c99af74a21cd6a1c713f76f554a4264f
psi <ledyba@users.sourceforge.jp>
2012-12-29 11:24:07 +0900

    traitsを使ってもっと簡便にした

--- src/donut/include/donut/native/Decoder.h
+++ src/donut/include/donut/native/Decoder.h
@@ -26,75 +26,102 @@ class Heap;
 
 namespace native {
 
+template <typename T, typename U=void>
+struct DecodeTraits {
+	typedef T return_type;
+};
+
+template <typename T>
+struct DecodeTraits<T, typename std::enable_if< std::is_reference<T>::value >::type> {
+	typedef typename DecodeTraits<typename std::remove_reference<T>::type >::return_type return_type;
+};
+
+template <typename T>
+struct DecodeTraits<T, typename std::enable_if< std::is_const<T>::value >::type> {
+	typedef typename DecodeTraits<typename std::remove_const<T>::type >::return_type return_type;
+};
+
 template <typename T>
 T decode(Handler<Heap> const& heap, Handler<Object> obj);
 
 //-----------------------------------------------------------------------------
 
-template <typename T, bool = IsBaseOf<T, Object>::result >
-struct PointerDecoder final {
-	static T* exec(Handler<Heap> const& heap, Handler<Object> obj) {
-		return decode<T*>( heap, obj );
-	}
-};
-
-template <typename T>
-struct PointerDecoder<T, true> final {
-	static T* exec(Handler<Heap> const& heap, Handler<Object> obj)
+template <typename T, typename U=void>
+struct Decoder final {
+	static typename DecodeTraits<T>::return_type exec(Handler<Heap> const& heap, Handler<Object> obj)
 	{
-		return obj.cast<T>().get();
+		return decode<typename DecodeTraits<T>::return_type>(heap, obj);
 	}
 };
 
-//-----------------------------------------------------------------------------
-
-template <typename T, bool = IsBaseOf<T, Object>::result, bool = IsBaseOf<Object, T>::result >
-struct HandlerDecoder final {
-	static Handler<T> exec(Handler<Heap> const& heap, Handler<Object> obj)
-	{
-		return decode<Handler<T> >( heap, obj );
-	}
-};
-
+//ポインタだが、Objectの派生でない
 template <typename T>
-struct HandlerDecoder<T, true, false> final {
-	static Handler<T> exec(Handler<Heap> const& heap, Handler<Object> obj)
+struct Decoder<T, typename std::enable_if<
+	std::is_pointer<typename DecodeTraits<T>::return_type>::value &&
+	!std::is_base_of<Object, typename std::remove_pointer<typename DecodeTraits<T>::return_type>::type
+		>::value >::type> final {
+	static typename DecodeTraits<T>::return_type exec(Handler<Heap> const& heap, Handler<Object> obj)
 	{
-		return obj.cast<T>();
+		return decode<typename DecodeTraits<T>::return_type>(heap, obj);
 	}
 };
 
+//ポインタでオブジェクトの派生
 template <typename T>
-struct HandlerDecoder<T, true, true> final {
-	static Handler<T> exec(Handler<Heap> const& heap, Handler<Object> obj)
+struct Decoder<T, typename std::enable_if<
+		std::is_pointer<typename DecodeTraits<T>::return_type>::value &&
+		std::is_base_of<Object, typename std::remove_pointer<typename DecodeTraits<T>::return_type>::type >::value
+			>::type> final {
+	static typename DecodeTraits<T>::return_type exec(Handler<Heap> const& heap, Handler<Object> obj)
+	{
+		return obj.cast<typename std::remove_pointer<typename DecodeTraits<T>::return_type>::type>().get();
+	}
+};
+
+//ポインタでオブジェクトそのもの
+template <typename T>
+struct Decoder<T, typename std::enable_if<
+	std::is_pointer<typename DecodeTraits<T>::return_type>::value &&
+	std::is_base_of<Object, typename std::remove_pointer<typename DecodeTraits<T>::return_type>::type>::value &&
+	std::is_base_of<typename std::remove_pointer<typename DecodeTraits<T>::return_type>::type, Object>::value >::type > final {
+	static typename DecodeTraits<T>::return_type exec(Handler<Heap> const& heap, Handler<Object> obj)
+	{
+		return obj.get();
+	}
+};
+//ハンドラでオブジェクトそのもの
+template <typename T>
+struct Decoder< Handler<T>, typename std::enable_if<
+		std::is_base_of<Object, typename DecodeTraits<T>::return_type >::value &&
+		std::is_base_of<typename DecodeTraits<T>::return_type,Object>::value
+			>::type > final {
+	static typename DecodeTraits< Handler<T> >::return_type exec(Handler<Heap> const& heap, Handler<Object> obj)
 	{
 		return obj;
 	}
 };
 
-//-----------------------------------------------------------------------------
-
+//ハンドラでオブジェクトの派生
 template <typename T>
-struct Decoder final {
-	static T exec(Handler<Heap> const& heap, Handler<Object> obj)
+struct Decoder< Handler<T>, typename std::enable_if<
+		std::is_base_of<Object, typename DecodeTraits<T>::return_type >::value &&
+		!std::is_base_of<typename DecodeTraits<T>::return_type,Object>::value >::type
+			> final {
+	static typename DecodeTraits<Handler<T> >::return_type exec(Handler<Heap> const& heap, Handler<Object> obj)
 	{
-		return decode<T>(heap, obj);
+		return obj.cast<T>();
 	}
 };
 
+//ハンドラでオブジェクトと全く関係ない
 template <typename T>
-struct Decoder<T*> final {
-	static T* exec(Handler<Heap> const& heap, Handler<Object> obj)
+struct Decoder< Handler<T>, typename std::enable_if<
+		!std::is_base_of<Object, typename DecodeTraits<T>::return_type >::value &&
+		!std::is_base_of<typename DecodeTraits<T>::return_type,Object>::value
+			>::type > final {
+	static typename DecodeTraits<Handler<T> >::return_type exec(Handler<Heap> const& heap, Handler<Object> obj)
 	{
-		return PointerDecoder<T>::exec(heap, obj);
-	}
-};
-
-template <typename T>
-struct Decoder< Handler<T> > final {
-	static Handler<T> exec(Handler<Heap> const& heap, Handler<Object> obj)
-	{
-		return HandlerDecoder<T>::exec(heap, obj);
+		return decode<Handler<T> >( heap, obj );
 	}
 };
 
