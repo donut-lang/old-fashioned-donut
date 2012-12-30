ef311cd65377e0e506033653aa20ab9d9952250e
psi <ledyba@users.sourceforge.jp>
2012-12-30 09:46:15 +0900

    いつの間にか要らなくなっていた

--- src/chisa/doc/dispose/TextContext.cpp
+++ src/chisa/doc/dispose/TextContext.cpp
@@ -31,10 +31,6 @@ TextContext::TextContext(Logger& log, Handler<RenderTree> renderTree)
 :log_(log)
 ,renderTree_(renderTree)
 ,font_()
-,face_(nullptr)
-,nullSurface_(cairo_image_surface_create(CAIRO_FORMAT_ARGB32, 1, 1))
-,nullOption_(cairo_font_options_create())
-,cairo_(cairo_create(nullSurface_))
 {
 	this->pushStyle(gl::TextDrawable::Style::Regular);
 	this->pushDeco(gl::TextDrawable::Decoration::None);
@@ -42,29 +38,10 @@ TextContext::TextContext(Logger& log, Handler<RenderTree> renderTree)
 	this->pushFont("");
 	this->pushColor(gl::Black);
 	this->pushBackColor(gl::Transparent);
-	{
-		cairo_status_t st = cairo_surface_status(this->nullSurface_);
-		if(st != CAIRO_STATUS_SUCCESS){
-			TARTE_EXCEPTION(Exception, "Oops. Failed to create cairo surface. Error code: %d", st);
-		}
-	}
-	{
-		cairo_status_t st = cairo_status(this->cairo_);
-		if(st != CAIRO_STATUS_SUCCESS){
-			TARTE_EXCEPTION(Exception, "Oops. Failed to create cairo. Error code: %d", st);
-		}
-	}
 }
 
 TextContext::~TextContext() noexcept
 {
-	if(this->face_){
-		cairo_font_face_destroy(this->face_);
-		this->face_ = nullptr;
-	}
-	cairo_font_options_destroy(this->nullOption_);
-	cairo_destroy(this->cairo_);
-	cairo_surface_destroy(this->nullSurface_);
 	this->font_.reset();
 	this->renderTree_.reset();
 }
@@ -143,12 +120,6 @@ void TextContext::pushFont( std::string const& name )
 {
 	this->fontStack_.push_back(name);
 	this->font_ = this->renderTree_->drawableManager()->queryFont(name);
-	if(this->face_){
-		cairo_font_face_destroy(this->face_);
-		this->face_ = nullptr;
-	}
-	gl::Font::RawFaceSession rfs(this->font_);
-	this->face_ = cairo_ft_font_face_create_for_ft_face(rfs.face(),0);
 
 }
 
@@ -159,17 +130,8 @@ std::string TextContext::nowFont() const
 
 void TextContext::popFont()
 {
-	if(!this->face_){
-		TARTE_EXCEPTION(Exception, "[BUG] Oops. you call \"popFont\" before pushing!");
-	}
-	cairo_font_face_destroy(this->face_);
-	this->face_ = nullptr;
-
-	this->fontStack_.pop_back();
 	const std::string name = this->fontStack_.back();
 	this->font_ = this->renderTree_->drawableManager()->queryFont(name);
-	gl::Font::RawFaceSession rfs(this->font_);
-	this->face_ = cairo_ft_font_face_create_for_ft_face(rfs.face(),0);
 }
 
 Handler<TextDrawableObject> TextContext::create(std::string const& strUtf8, Node* parentNode, const float relDepth )
