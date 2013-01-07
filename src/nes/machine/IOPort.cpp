fa9e0cb231921f247dc64d0eff1be5899af33478
psi <ledyba@users.sourceforge.jp>
2013-01-07 14:46:25 +0900

    ioPort追加

---
+++ src/nes/machine/IOPort.cpp
@@ -0,0 +1,17 @@
+#include "VirtualMachine.h"
+
+namespace nes {
+
+IOPort::IOPort(VirtualMachine& vm, GamepadFairy* pad1, GamepadFairy* pad2)
+:vm_(vm)
+,debugger_(vm.debugger())
+,pad1Fairy(pad1 == 0 ? dummyPad : *pad1)
+,pad2Fairy(pad2 == 0 ? dummyPad : *pad2)
+,pad1Idx(GamepadFairy::A)
+,pad2Idx(GamepadFairy::A)
+{
+
+}
+
+
+}
