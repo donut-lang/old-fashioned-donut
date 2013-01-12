642b6ab0c0e38368cbd8e2a3395b88bf37483da7
psi <ledyba@users.sourceforge.jp>
2013-01-12 10:29:40 +0900

    コピペして置き換えという負け感

---
+++ src/chisa/tk/donut/ServantObject.cpp
@@ -0,0 +1,239 @@
+/**
+ * Chisa
+ * Copyright (C) 2012 psi
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <functional>
+#include "ServantObject.h"
+#include "ElementObject.h"
+
+namespace chisa {
+namespace tk {
+
+const static std::string TAG("ServantObject");
+
+/**********************************************************************************************************************
+ * Base
+ **********************************************************************************************************************/
+
+ServantProvider::ServantProvider(Handler<Heap> const& heap, std::string const& provname, Handler<Heaven> const& heaven)
+:ReactiveProvider(heap, provname)
+,heaven_(heaven)
+{
+}
+
+Handler<Heaven> ServantProvider::heaven() const
+{
+	return heaven_.lock();
+}
+
+//---------------------------------------------------------
+
+ServantObject::ServantObject(ServantProvider* provider)
+:ReactiveNativeObject(provider)
+{
+}
+
+void ServantObject::bootstrap(const Handler<Heap>& heap, Handler<Servant> const& angelTarget)
+{
+	this->ReactiveNativeObject::bootstrap(heap);
+	this->target_ = angelTarget;
+}
+
+Handler<World> ServantObject::world() const
+{
+	return target_->world();
+}
+
+Handler<Servant> ServantObject::angelTarget() const
+{
+	return this->target_;
+}
+
+
+/**********************************************************************************************************************
+ * BaseT
+ **********************************************************************************************************************/
+template <typename ProviderT, typename ObjectT, typename TargetT, typename AntiT>
+ServantProviderBaseT<ProviderT, ObjectT, TargetT, AntiT>::ServantProviderBaseT(Handler<Heap> const& heap, std::string const& provname, Handler<Heaven> const& heaven)
+:ServantProvider(heap, provname, heaven)
+{
+
+}
+
+//---------------------------------------------------------
+
+template<typename ProviderT, typename ObjectT, typename TargetT, typename AntiT>
+inline ServantObjectBaseT<ProviderT, ObjectT, TargetT, AntiT>::ServantObjectBaseT(ProviderT* provider)
+:ServantObject(provider)
+{
+}
+
+template <typename ProviderT, typename ObjectT, typename TargetT, typename AntiT>
+Handler<TargetT> ServantObjectBaseT<ProviderT, ObjectT, TargetT, AntiT>::angelTarget() const
+{
+	return Handler<TargetT>::__internal__fromRawPointerWithoutCheck( static_cast<TargetT*>(ServantObject::angelTarget().get()) );
+}
+
+
+template <typename ProviderT, typename ObjectT, typename TargetT, typename AntiT>
+void ServantObjectBaseT<ProviderT, ObjectT, TargetT, AntiT>::bootstrap(const Handler<Heap>& heap, const Handler<TargetT>& angelTarget)
+{
+	this->ServantObject::bootstrap(heap, angelTarget);
+}
+
+template <typename ProviderT, typename ObjectT, typename AngelT, typename AntiT>
+void ServantObjectBaseT<ProviderT, ObjectT, AngelT, AntiT>::onFutureDiscarded(Handler<Heap> const& heap)
+{
+
+}
+template <typename ProviderT, typename ObjectT, typename AngelT, typename AntiT>
+void ServantObjectBaseT<ProviderT, ObjectT, AngelT, AntiT>::onHistoryDiscarded(Handler<Heap> const& heap)
+{
+
+}
+
+template<typename ProviderT, typename ObjectT, typename AngelT, typename AntiT>
+inline typename ServantObjectBaseT<ProviderT, ObjectT, AngelT, AntiT>::ResultType ServantObjectBaseT<ProviderT, ObjectT, AngelT, AntiT>::execAntiSideEffect(const Handler<Heap>& heap, const AntiSideEffect& val)
+{
+	AntiT newAnti;
+	return std::make_tuple(true, newAnti);
+}
+
+template<typename ProviderT, typename ObjectT, typename AngelT, typename AntiT>
+inline typename ServantObjectBaseT<ProviderT, ObjectT, AngelT, AntiT>::ResultType ServantObjectBaseT<ProviderT, ObjectT, AngelT, AntiT>::onBack(const Handler<Heap>& heap, const AntiSideEffect& val)
+{
+	return this->execAntiSideEffect(heap, val);
+}
+
+template<typename ProviderT, typename ObjectT, typename AngelT, typename AntiT>
+inline typename ServantObjectBaseT<ProviderT, ObjectT, AngelT, AntiT>::ResultType ServantObjectBaseT<ProviderT, ObjectT, AngelT, AntiT>::onForward(const Handler<Heap>& heap, const AntiSideEffect& val)
+{
+	return this->execAntiSideEffect(heap, val);
+}
+
+/**********************************************************************************************************************
+ * Element
+ **********************************************************************************************************************/
+
+ElementServantProvider::ElementServantProvider(const Handler<Heap>& heap, const Handler<Heaven>& heaven)
+:Super(heap, "ElementServant", heaven)
+{
+}
+
+//---------------------------------------------------------
+
+ElementServantObject::ElementServantObject(ElementServantProvider* provider)
+:Super(provider)
+{
+}
+
+void ElementServantObject::bootstrap(const Handler<Heap>& heap, const Handler<ElementServant>& angeltarget)
+{
+	Super::bootstrap(heap, angeltarget);
+}
+
+void ElementServantObject::onFutureDiscarded(const Handler<Heap>& heap)
+{
+	Super::onFutureDiscarded(heap);
+}
+
+void ElementServantObject::onHistoryDiscarded(const Handler<Heap>& heap)
+{
+	Super::onHistoryDiscarded(heap);
+}
+
+ElementServantObject::ResultType ElementServantObject::onBack(const Handler<Heap>& heap, const AntiSideEffect& val)
+{
+	return Super::onBack(heap, val);
+}
+
+ElementServantObject::ResultType ElementServantObject::onForward(const Handler<Heap>& heap, const AntiSideEffect& val)
+{
+	return Super::onForward(heap, val);
+}
+
+XValue ElementServantObject::saveImpl(const Handler<Heap>& heap)
+{
+}
+
+void ElementServantObject::loadImpl(const Handler<Heap>& heap, const XValue& data)
+{
+}
+
+std::string ElementServantObject::reprImpl(const Handler<Heap>& heap) const
+{
+	return ::tarte::format("(ElementServantObject: %p)", this);
+}
+
+
+/**********************************************************************************************************************
+ * Widget
+ **********************************************************************************************************************/
+
+HaloServantProvider::HaloServantProvider(const Handler<Heap>& heap, const Handler<Heaven>& heaven)
+:Super(heap, "HaloServant", heaven)
+{
+}
+
+//---------------------------------------------------------
+
+HaloServantObject::HaloServantObject(HaloServantProvider* provider)
+:Super(provider)
+{
+}
+
+void HaloServantObject::bootstrap(const Handler<Heap>& heap, const Handler<HaloServant>& angelTarget)
+{
+	Super::bootstrap(heap, angelTarget);
+}
+
+void HaloServantObject::onFutureDiscarded(const Handler<Heap>& heap)
+{
+	Super::onFutureDiscarded(heap);
+}
+
+void HaloServantObject::onHistoryDiscarded(const Handler<Heap>& heap)
+{
+	Super::onHistoryDiscarded(heap);
+}
+
+HaloServantObject::ResultType HaloServantObject::onBack(const Handler<Heap>& heap, const AntiSideEffect& val)
+{
+	return Super::onBack(heap, val);
+}
+
+HaloServantObject::ResultType HaloServantObject::onForward(const Handler<Heap>& heap, const AntiSideEffect& val)
+{
+	return Super::onForward(heap, val);
+}
+
+XValue HaloServantObject::saveImpl(const Handler<Heap>& heap)
+{
+}
+
+void HaloServantObject::loadImpl(const Handler<Heap>& heap, const XValue& data)
+{
+}
+
+std::string HaloServantObject::reprImpl(const Handler<Heap>& heap) const
+{
+	return ::tarte::format("(HaloServantObject: %p)", this);
+}
+
+}}
+
+
