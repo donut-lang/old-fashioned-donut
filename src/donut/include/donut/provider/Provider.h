2fbdd7b0e22f7806ede4f0b60b5dbe5453fa092c
psi <ledyba@users.sourceforge.jp>
2013-01-14 10:14:58 +0900

    unlikely

--- src/donut/include/donut/provider/Provider.h
+++ src/donut/include/donut/provider/Provider.h
@@ -50,7 +50,7 @@ template <typename ObjectT, typename ProviderT, typename... Args>
 inline Handler<ObjectT> HeapProvider::newInstance(ProviderT* self, Args... args)
 {
 	Handler<Heap> heap(self->heap().lock());
-	if(!heap) {
+	if( unlikely(!heap) ) {
 		DONUT_EXCEPTION(Exception, "[BUG] Heap is already dead.");
 	}
 	return heap->createObject<ObjectT>(self, args...);
