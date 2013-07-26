037fc972d8235cc8fa4221e25cada875a73cf33b
psi <ledyba@users.sourceforge.jp>
2013-07-26 09:20:48 +0900

    add default

---
+++ test/main.cpp
@@ -0,0 +1,13 @@
+/* coding: utf-8 */
+/**
+ * TestEntryPoint
+ *
+ * Copyright 2013, psi
+ */
+
+#include <gtest/gtest.h>
+int main(int argc, char **argv) {
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
+
